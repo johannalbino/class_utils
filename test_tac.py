5f96b26e43a81fb020ae779ce462d28e139c68ac
Johann Albino <johann.albino.ti@gmail.com>
2019-12-20 09:29:05 -0300

    Add class Treating Accent in Characters (tac)

--- test_tac.py
+++ test_tac.py
@@ -1,6 +1,6 @@
-from tsc import TreatingAccentsCharacters
+from tac import TreatingAccentsCharacters
 tac = TreatingAccentsCharacters()
 
-text = 'Eu não sou mulher, mas é muito bom ter uma mulher por perto, tipo você!'
+text = 'Eu não sou mulher, mas é muito bom ter uma mulher por perto, tipo você! ü'
 
 print(tac.tac(text))
